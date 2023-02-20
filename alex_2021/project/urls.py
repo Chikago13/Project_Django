1206566f61320a171bc0b59b2112846227f8a7e0
Alex <sivko2201@gmail.com>
2023-02-21 00:34:59 +0300

    New_project_django

--- alex_2021/project/urls.py
+++ alex_2021/project/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('project/', views.project),
+    path("",views.project_index, name="project_index"),
+    path("<int:pk>/",views.project_detail, name="project_detail"),
 
 ]
