23f72f0d32eb8ebf2af13ce165b87a82a8fdc93d
Alex <sivko2201@gmail.com>
2023-02-21 19:16:44 +0300

    New_project_django_blog

---
+++ alex_2021/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Post, Comment
+
+def blog_index(request):
+    posts= Post.objects.all().order_by('-created_on')
+    context = {
+        'posts': posts,
+    }
+    return  render(request, "blog_index.html", context)
+
+def blog_detail(request):
+    post = Post.objects.get(pk = pk)
+    comment = Comment.objects.filter(post = post)
+    context = {
+        'post': post,
+        'comment': comment
+    }
+    return render(request, "blog_detail.html", context)
